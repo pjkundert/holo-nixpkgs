7e0e089c683757b2541d76e9648593cd9fd42748
Alastair Ong <alastair.ong@gmail.com>
2020-08-25 15:28:41 +0100

    profiles/hydra: add new cache keys

--- profiles/binary-cache.nix
+++ profiles/binary-cache.nix
@@ -5,5 +5,7 @@
 
   nix.binaryCachePublicKeys = [
     "cache.holo.host-1:lNXIXtJgS9Iuw4Cu6X0HINLu9sTfcjEntnrgwMQIMcE="
+    "cache.holo.host-2:ZJCkX3AUYZ8soxTLfTb60g+F3MkWD7hkH9y8CgqwhDQ="
+  ];
   ];
 }
