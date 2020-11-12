678f893952c2a2be6652f134d3c795216eb0acce
Robbie Carlton <robbie.carlton@gmail.com>
2020-11-12 14:32:46 -0700

    remove proxypass console server

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -144,7 +144,6 @@ in
 
     virtualHosts.localhost = {
         locations."/".proxyPass = "http://unix:/run/hpos-admin.sock:/";
-        locations."/".proxyPass = "http://unix:/run/host-console-server.sock:/";
       };
 
     appendHttpConfig = ''
