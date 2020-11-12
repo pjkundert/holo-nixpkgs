12673378b069eb52c066cf663d21414260a66366
Robbie Carlton <robbie.carlton@gmail.com>
2020-11-12 14:02:05 -0700

    update default.nix

--- overlays/holo-nixpkgs/host-console-server/default.nix
+++ overlays/holo-nixpkgs/host-console-server/default.nix
@@ -1,51 +1,28 @@
-{ stdenv
-, rustPlatform
-, fetchFromGitHub
-, makeWrapper
-, nodejs
-, node2nix
-, ps
-, python
-, fetchgit
-}:
+{ stdenv, fetchFromGitHub, nodejs, npmToNix }:
 
-stdenv.mkDerivation rec {
-  name = "host-console-server";
-  src = gitignoreSource ./.;
+{
+  host-console-server = stdenv.mkDerivation rec {
+    name = "host-console-server";
+    src = gitignoreSource ./.;
 
-  nativeBuildInputs = [
-    makeWrapper
-    nodejs
-    # REVIEW: why do we need this? ask @mjbrisebois
-    ps
-  ];
+    buildInputs = [ nodejs ];
 
-  preConfigure = ''
-    cp -r ${node2nix { inherit src; }} node_modules
-    chmod -R +w node_modules
-    patchShebangs node_modules
-  '';
+    preConfigure = ''
+      cp -r ${npmToNix { src = "${src}/"; }} node_modules
+      chmod -R +w node_modules
+      chmod +x node_modules/.bin/webpack
+      patchShebangs node_modules
+    '';
 
-  buildPhase = ''
-    npm run build
-  '';
+    buildPhase = ''
+      npm run build
+    '';
 
-  installPhase = ''
-      mkdir $out
-      mv build node_modules rpc-websocket-wrappers server.js $out
-      makeWrapper ${nodejs}/bin/node $out/bin/${name} \
-        --add-flags $out/server.js
-  '';
+    installPhase = ''
+      cp -r dist/ $out
+    '';
 
-  fixupPhase = ''
-    patchShebangs $out
-  '';
-
-  checkPhase = ''
-      make test-nix
-      make stop-sim2h
-  '';
-
-  # HACK: consider flipping it on when test timeout issues are resolved
-  doCheck = false;
+    doCheck = false;
+    meta.platforms = stdenv.lib.platforms.linux;
+  };
 }
