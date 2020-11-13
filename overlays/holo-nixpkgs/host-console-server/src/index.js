cfc0a38274587ea90f501d6669b9802ce6e960c6
Robbie Carlton <robbie.carlton@gmail.com>
2020-11-12 21:04:17 -0700

    use appInfo to get cellId

--- overlays/holo-nixpkgs/host-console-server/src/index.js
+++ overlays/holo-nixpkgs/host-console-server/src/index.js
@@ -1,41 +1,35 @@
 const express = require('express')
 const app = express()
-const { AdminWebsocket, AppWebsocket } = require('@holochain/conductor-api')
+const { AppWebsocket } = require('@holochain/conductor-api')
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
 const argv = yargs(hideBin(process.argv)).argv
-
-if (!argv.adminPort) {
-  throw new Error('Host console server requires --admin-port option.')
-}
 
 if (!argv.appPort) {
   throw new Error('Host console server requires --app-port option.')
 }
 
-if (!argv.hhaHash) {
-  throw new Error('Host console server requires --hha-hash option.')
+if (!argv.appId) {
+  throw new Error('Host console server requires --app-id option.')
 }
 
 const UNIX_SOCKET = '/run/host-console-server.sock'
 
-// TODO: write these functions
-const cellIdhasDnaHash = dnaHash => cellId => true
-const agentKeyFromCellId = cellId => cellId[0]
-
 app.get('/hosted_happs', async (_, res) => {
-  const adminWebsocket = await AdminWebsocket.connect(`ws://localhost:${argv.adminPort}`)
-
-  const cellIds = await adminWebsocket.listCellIds()
-
-  const hhaCellId = cellIds.find(cellIdhasDnaHash(argv.hhaHash))
-
-  const agentKey = agentKeyFromCellId(hhaCellId)
-
   const appWebsocket = await AppWebsocket.connect(`ws://localhost:${argv.appPort}`)
 
+  const appInfo = appWebsocket.appInfo({ app_id: argv.appId })
+
+  if (!appInfo) {
+    throw new Error (`Couldn't find Holo Hosting App with id ${ argv.appId }`)
+  }
+
+  const cellId = appInfo.cell_data[0][0]
+
+  const agentKey = cellId[1]
+
   const happs = await appWebsocket.callZome({
-    cellId: hhaCellId,
+    cellId,
     zome_name: 'hha',
     fn_name: 'get_happs',
     provenance: agentKey,
