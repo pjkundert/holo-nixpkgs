57425e3160bc3c00155acca53fbc64bcedd34aed
Alastair Ong <alastair.ong@holo.host>
2020-11-19 09:40:28 +0000

    profiles/installers: Add installers

---
+++ profiles/installers/holoport-nano/default.nix
@@ -0,0 +1,34 @@
+{ config, pkgs, ... }:
+
+let
+  nixpkgs = import ../../../nixpkgs/source.nix;
+in
+
+{
+  imports = [
+    "${nixpkgs}/nixos/modules/installer/cd-dvd/sd-image.nix"
+    ../../hardware/holoport-nano
+    ../.
+  ];
+  
+  nix.package = let
+    nixpkgs-1909-latest = import (fetchTarball https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.09.tar.gz) {};
+  in nixpkgs-1909-latest.nix;
+  
+  environment.systemPackages = with pkgs; [ git ];
+
+  sdImage.imageName = "${config.system.build.baseName}.img";
+
+  sdImage.populateFirmwareCommands = ''
+    dd conv=notrunc if=${pkgs.ubootBananaPim64}/u-boot-sunxi-with-spl.bin of=$img bs=8k seek=1
+  '';
+
+  sdImage.populateRootCommands = with pkgs; ''
+    mkdir -p ./files/boot
+    ${buildPackages.extlinux-conf-builder} \
+      -b ${holoport-nano-dtb} \
+      -c ${config.system.build.toplevel} \
+      -d ./files/boot \
+      -t 1
+  '';
+}
