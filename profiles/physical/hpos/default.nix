730fcc8143a0c03bbac327ab7aa535469ce8b94e
Alastair Ong <alastair.ong@gmail.com>
2020-11-27 17:24:18 +0000

    profiles/physical: remove conflicting declaration for enableRedistributableFirmware

--- profiles/physical/hpos/default.nix
+++ profiles/physical/hpos/default.nix
@@ -6,7 +6,7 @@
   # https://github.com/NixOS/nixpkgs/issues/71955#issuecomment-546189168
   boot.enableContainers = false;
 
-  hardware.enableRedistributableFirmware = lib.mkDefault false;
+  hardware.enableRedistributableFirmware = lib.mkForce false;
 
   services.hpos-led-manager.enable = lib.mkDefault true;
 }
