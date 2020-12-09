6f38b2935d93bccf11b7830ff79c771128d6ba68
Alastair Ong <alastair.ong@gmail.com>
2020-12-09 09:27:27 +0000

    Update profiles/physical/hpos/default.nix
    Co-authored-by: peeech <pj@imagine-nyc.com>

--- profiles/physical/hpos/default.nix
+++ profiles/physical/hpos/default.nix
@@ -6,7 +6,10 @@
   # https://github.com/NixOS/nixpkgs/issues/71955#issuecomment-546189168
   boot.enableContainers = false;
 
-  hardware.enableRedistributableFirmware = lib.mkForce false; # Needed because hardware-configuration.nix generated on 20.09 install has conflict. Old HoloPorts do not have this conflict as hardware-configuration is still 19.09 version (does not get updated on `nixos-rebuild switch`)
+  # Needed because hardware-configuration.nix generated on 20.09 install has conflict.
+  # Old HoloPorts do not have this conflict as hardware-configuration is still 19.09 version
+  # and does not get updated on `nixos-rebuild switch`
+  hardware.enableRedistributableFirmware = lib.mkForce false;
 
   services.hpos-led-manager.enable = lib.mkDefault true;
 }
