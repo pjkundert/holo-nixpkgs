ccd99ee9e9b23773eece6f32f4c020304e6a2025
peeech <pj@imagine-nyc.com>
2021-01-20 09:17:49 -0600

    overlays/configure-holochain: bump to rev cfbd0ca

--- overlays/holo-nixpkgs/configure-holochain/default.nix
+++ overlays/holo-nixpkgs/configure-holochain/default.nix
@@ -7,8 +7,8 @@ rustPlatform.buildRustPackage {
   src = fetchFromGitHub {
     owner = "Holo-Host";
     repo = "hpos-configure-holochain";
-    rev = "0d9d6dca334e43179814b8a909ecf3fe26b50325";
-    sha256 = "1ggyl1wrf7g1k155xfyp5qv7mx17jh8lgq25pnl98qp95zk7yjpv";
+    rev = "cfbd0ca7af6e9efc5ad2ef1d97a9b0b054fc52ba";
+    sha256 = "1aic7vwasfjz3crnjnf355jqpfi3s7yqsxb6rbn6xb1sgvz1578g";
   };
 
   cargoSha256 = "178bd64if1wajmlna2ws7687majwwc1gx7xl1dl8i0ss17mx2wj0";
