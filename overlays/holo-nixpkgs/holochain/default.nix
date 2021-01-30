41ca402b37850efe9d0e395989bdcf6b1c760fb2
Eric Harris-Braun <eric@harris-braun.com>
2021-01-30 14:04:01 -0500

    Bump holochain for thiserror 1.0.23

--- overlays/holo-nixpkgs/holochain/default.nix
+++ overlays/holo-nixpkgs/holochain/default.nix
@@ -2,10 +2,10 @@
 
 rec {
   mkHolochainBinary = {
-      version ? "2021-01-29"
-      , rev ? "9b09d7acba71739ab4bdb26f43cf210d5f298165"
-      , sha256 ? "1s0mhanbij9w5hyyikqvr0w3pgwsr8xhqlbmfn34csvb17lqgd27"
-      , cargoSha256 ? "0sd60hwjfbwspdkdcwj9mgbh7bx31n1qijbgy91zri5l6z41bwcn"
+      version ? "2021-01-30"
+      , rev ? "9de90306ccd8511d9c1bae6a755df7c10137d8df"
+      , sha256 ? "1d5mkracc11rjmix5mxxk6qs384wzi0an3768crwrcqbgpavyasg"
+      , cargoSha256 ? "0gi730yqmbilb0zzi3scvp7riv30hjn66xwhknhcc0dwmxyc8nns"
       , crate
       , ... } @ overrides: rustPlatform.buildRustPackage (lib.attrsets.recursiveUpdate {
     name = "holochain";
