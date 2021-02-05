6bc843368038a8860288c464a5d3d4f8df21ac0f
Alastair Ong <alastair.ong@holo.host>
2021-02-05 10:40:08 +0000

    profiles/hpos: change from core_happ download from dna_path to dna_url

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -212,21 +212,21 @@ in
           app_id = "core-happs";
           uuid = "0001";
           version = "alpha9";
-          /* dna_url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha6/holo-hosting-app.dna.gz"; */
-          dna_path = builtins.fetchurl {
-            url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha9/holo-hosting-app.dna.gz";
-            sha256 = "0z94sg70xi0p3sybi6w1i7rbrnj8pv60m91ybizma0wc5jwmlnq3"; # To get sha run `nix-prefetch-url URL`
-          };
+          dna_url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha9/holo-hosting-app.dna.gz";
+          # dna_path = builtins.fetchurl {
+          #   url = "https://holo-host.github.io/holo-hosting-app-rsm/releases/downloads/v0.0.1-alpha9/holo-hosting-app.dna.gz";
+          #   sha256 = "0z94sg70xi0p3sybi6w1i7rbrnj8pv60m91ybizma0wc5jwmlnq3"; # To get sha run `nix-prefetch-url URL`
+          # };
         }
         {
           app_id = "servicelogger";
           uuid = "0001";
           version = "alpha4";
-          /* dna_url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha4/servicelogger.dna.gz"; */
-          dna_path = builtins.fetchurl {
-            url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha4/servicelogger.dna.gz";
-            sha256 = "11w0a1fmm4js9i298ahzmwswv7sza2n0rv9nshl76nl0zi37bdi0"; # To get sha run `nix-prefetch-url URL`
-          };
+          dna_url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha4/servicelogger.dna.gz";
+          # dna_path = builtins.fetchurl {
+          #   url = "https://holo-host.github.io/servicelogger-rsm/releases/downloads/v0.0.1-alpha4/servicelogger.dna.gz";
+          #   sha256 = "11w0a1fmm4js9i298ahzmwswv7sza2n0rv9nshl76nl0zi37bdi0"; # To get sha run `nix-prefetch-url URL`
+          # };
         }
       ];
       self_hosted_happs = [
