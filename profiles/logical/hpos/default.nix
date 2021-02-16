19970b34ce5fb0c3b1ea53ee96d701c8678c53ad
Eric Harris-Braun <eric@harris-braun.com>
2021-02-16 12:41:25 -0500

    Bump to ec ui alpha29

--- profiles/logical/hpos/default.nix
+++ profiles/logical/hpos/default.nix
@@ -224,7 +224,7 @@ in
           app_id = "elemental-chat";
           uuid = "develop";
           version = "alpha19";
-          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha28/elemental-chat-for-dna-alpha19-develop.zip";
+          ui_url = "https://github.com/holochain/elemental-chat-ui/releases/download/v0.0.1-alpha29/elemental-chat-for-dna-alpha19-develop.zip";
           dna_url = "https://github.com/holochain/elemental-chat/releases/download/v0.0.1-alpha19/elemental-chat.dna.gz";
         }
       ];
