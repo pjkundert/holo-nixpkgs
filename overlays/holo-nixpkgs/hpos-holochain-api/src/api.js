92b1b31d6eaeaa31fd97050cd1ee4971babe0e4a
Robbie Carlton <robbie.carlton@gmail.com>
2021-02-22 10:13:21 -0700

    hpos-holochain-api conforms to standard js

--- overlays/holo-nixpkgs/hpos-holochain-api/src/api.js
+++ overlays/holo-nixpkgs/hpos-holochain-api/src/api.js
@@ -1,136 +1,135 @@
-const { ADMIN_PORT, HAPP_PORT, getAppIds } = require("./const")
-const { AdminWebsocket, AppWebsocket } = require("@holochain/conductor-api")
+const { ADMIN_PORT, HAPP_PORT, getAppIds } = require('./const')
+const { AdminWebsocket, AppWebsocket } = require('@holochain/conductor-api')
 const { downloadFile } = require('./utils')
 const msgpack = require('@msgpack/msgpack')
 
 // NOTE: this code assumes a single DNA per hApp.  This will need to be updated when the hApp bundle
 // spec is completed, and the hosted-happ config Yaml file will also need to be likewise updated
 const installHostedHapp = async (happId, dna, agentPubKey, serviceloggerPref) => {
-    console.log("Installing DNA...", dna);
-    // How to install a DNA
-      // We need to download the DNA to a perticular location.
-      // Use that location and install
-    // NOTE: we also have to install a servicelogger instance
-      // We need to know the path to the servicelogger
-      // Use that servicelogger and install a new DNA with the properties set as
-      // { properties: Array.from(msgpack.encode({"bound_dna_id":"uhC0kmrkoAHPVf_eufG7eC5fm6QKrW5pPMoktvG5LOC0SnJ4vV1Uv"})) }
+  console.log('Installing DNA...', dna)
+  // How to install a DNA
+  // We need to download the DNA to a perticular location.
+  // Use that location and install
+  // NOTE: we also have to install a servicelogger instance
+  // We need to know the path to the servicelogger
+  // Use that servicelogger and install a new DNA with the properties set as
+  // { properties: Array.from(msgpack.encode({"bound_dna_id":"uhC0kmrkoAHPVf_eufG7eC5fm6QKrW5pPMoktvG5LOC0SnJ4vV1Uv"})) }
 
-    try {
-        // Install via admin interface
-        console.log("Connecting to admin port...");
-        const adminWebsocket = await AdminWebsocket.connect(
-            `ws://localhost:${ADMIN_PORT}`
-        );
-        console.log("Downloading DNA URL...");
-        let payloadDna = []
-        for(let i=0; i < dna.length; i++) {
-          const dnaPath = await downloadFile(dna[i].src_url);
-          const registeredHash = await adminWebsocket.registerDna({
-            source: { path: dnaPath }
-          })
-          payloadDna.push({
-                nick: dna[i].nick,
-                hash: registeredHash
-          })
-        }
-        const payload = {
-            agent_key: agentPubKey,
-            installed_app_id: happId,
-            dnas: payloadDna,
-        }
-        console.log("Installing happ: ", payload);
-        const installed_app = await adminWebsocket.installApp(payload);
-        console.log("Activate happ...", installed_app);
-
-        // Install servicelogger instance
-        await installServicelogger(adminWebsocket, happId, serviceloggerPref)
-
-        await adminWebsocket.activateApp({ installed_app_id: installed_app.installed_app_id });
-        console.log(`Successfully installed ${happId} (read-only instance and service logger) for key ${agentPubKey.toString('base64')}`);
-    } catch(e) {
-        console.log(`Failed to install dna ${dna.nick} with error: `, e);
-        throw new Error(`Failed to install dna ${dna.nick} with error: `, e);
+  try {
+    // Install via admin interface
+    console.log('Connecting to admin port...')
+    const adminWebsocket = await AdminWebsocket.connect(
+      `ws://localhost:${ADMIN_PORT}`
+    )
+    console.log('Downloading DNA URL...')
+    const payloadDna = []
+    for (let i = 0; i < dna.length; i++) {
+      const dnaPath = await downloadFile(dna[i].src_url)
+      const registeredHash = await adminWebsocket.registerDna({
+        source: { path: dnaPath }
+      })
+      payloadDna.push({
+        nick: dna[i].nick,
+        hash: registeredHash
+      })
     }
+    const payload = {
+      agent_key: agentPubKey,
+      installed_app_id: happId,
+      dnas: payloadDna
+    }
+    console.log('Installing happ: ', payload)
+    const installedApp = await adminWebsocket.installApp(payload)
+    console.log('Activate happ...', installedApp)
+
+    // Install servicelogger instance
+    await installServicelogger(adminWebsocket, happId, serviceloggerPref)
+
+    await adminWebsocket.activateApp({ installed_app_id: installedApp.installed_app_id })
+    console.log(`Successfully installed ${happId} (read-only instance and service logger) for key ${agentPubKey.toString('base64')}`)
+  } catch (e) {
+    console.log(`Failed to install dna ${dna.nick} with error: `, e)
+    throw new Error(`Failed to install dna ${dna.nick} with error: `, e)
+  }
 }
 
 const installServicelogger = async (adminWebsocket, happId, preferences) => {
-  console.log(`Staring installation process of servicelogger for hosted happ {${happId}}`);
+  console.log(`Staring installation process of servicelogger for hosted happ {${happId}}`)
   const appWebsocket = await AppWebsocket.connect(
-      `ws://localhost:${HAPP_PORT}`
-  );
+    `ws://localhost:${HAPP_PORT}`
+  )
   // TODO: Get servicelogger appID
   const APP_ID = await getAppIds()
-  const cell = await appWebsocket.appInfo({installed_app_id: APP_ID.SL})
+  const cell = await appWebsocket.appInfo({ installed_app_id: APP_ID.SL })
   const serviceloggerDnaHash = cell.cell_data[0][0][0]
   const hostPubKey = cell.cell_data[0][0][1]
 
-  const installed_app_id = `${happId}::servicelogger`
-  console.log(`Registring ${installed_app_id}...`);
+  const installedAppId = `${happId}::servicelogger`
+  console.log(`Registring ${installedAppId}...`)
 
   const registeredHash = await adminWebsocket.registerDna({
     source: {
-      hash: serviceloggerDnaHash,
+      hash: serviceloggerDnaHash
     },
-    properties: Array.from(msgpack.encode({"bound_happ_id": happId}))
+    properties: Array.from(msgpack.encode({ bound_happ_id: happId }))
   })
 
-  console.log(`Installing ${installed_app_id}...`)
-  const installed_app = await adminWebsocket.installApp({
-      agent_key: hostPubKey,
-      installed_app_id,
-      dnas: [{
-            nick: "servicelogger",
-            hash: registeredHash
-      }],
-  });
+  console.log(`Installing ${installedAppId}...`)
+  await adminWebsocket.installApp({
+    agent_key: hostPubKey,
+    installed_app_id: installedAppId,
+    dnas: [{
+      nick: 'servicelogger',
+      hash: registeredHash
+    }]
+  })
 
-  console.log(`Activating ${installed_app_id}...`)
-  await adminWebsocket.activateApp({ installed_app_id });
-  return await callZome(appWebsocket, installed_app_id, "service", "set_logger_settings", preferences )
+  console.log(`Activating ${installedAppId}...`)
+  await adminWebsocket.activateApp({ installedAppId })
+  return callZome(appWebsocket, installedAppId, 'service', 'set_logger_settings', preferences)
 }
 
 const createAgent = async (adminWebsocket) => {
-    try {
-        let agentPubKey = await adminWebsocket.generateAgentPubKey();
-        console.log(`Generated new agent ${agentPubKey.toString('base64')}`);
-        return agentPubKey;
-    } catch(e) {
-        console.log(`Error while generating new agent: ${e.message}.`);
-    }
+  try {
+    const agentPubKey = await adminWebsocket.generateAgentPubKey()
+    console.log(`Generated new agent ${agentPubKey.toString('base64')}`)
+    return agentPubKey
+  } catch (e) {
+    console.log(`Error while generating new agent: ${e.message}.`)
+  }
 }
 
 const listInstalledApps = async (adminWebsocket) => {
-    try {
-        const apps = await adminWebsocket.listActiveApps();
-        console.log("listActiveApps app result: ", apps)
-        return apps
-    } catch(e) {
-        console.error(`Failed to get list of active happs with error: `, e);
-        return;
-    }
+  try {
+    const apps = await adminWebsocket.listActiveApps()
+    console.log('listActiveApps app result: ', apps)
+    return apps
+  } catch (e) {
+    console.error(`Failed to get list of active happs with error: `, e)
+  }
 }
 
 const startHappInterface = async (adminWebsocket) => {
-    try {
-        console.log(`Starting app interface on port ${HAPP_PORT}`);
-        await adminWebsocket.attachAppInterface({ port: HAPP_PORT });
-    } catch(e) {
-        console.log(`Error: ${e.message}, probably interface already started.`);
-    }
+  try {
+    console.log(`Starting app interface on port ${HAPP_PORT}`)
+    await adminWebsocket.attachAppInterface({ port: HAPP_PORT })
+  } catch (e) {
+    console.log(`Error: ${e.message}, probably interface already started.`)
+  }
 }
 
-const callZome = async (ws, installed_app_id, zome_name, fn_name, payload ) => {
-  const appInfo = await ws.appInfo({ installed_app_id })
+const callZome = async (ws, installedAppId, zomeName, fnName, payload) => {
+  const appInfo = await ws.appInfo({ installed_app_id: installedAppId })
 
   if (!appInfo) {
-    throw new Error(`Couldn't find Holo Hosting App with id ${installed_app_id}`)
+    throw new Error(`Couldn't find Holo Hosting App with id ${installedAppId}`)
   }
   const cellId = appInfo.cell_data[0][0]
   const agentKey = cellId[1]
-  return await ws.callZome({
+  return ws.callZome({
     cell_id: cellId,
-    zome_name,
-    fn_name,
+    zome_name: zomeName,
+    fn_name: fnName,
     provenance: agentKey,
     payload
   })
--- overlays/holo-nixpkgs/hpos-holochain-api/src/index.js
+++ overlays/holo-nixpkgs/hpos-holochain-api/src/index.js
@@ -5,54 +5,54 @@ const express = require('express')
 const app = express()
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
 const { UNIX_SOCKET, HAPP_PORT, ADMIN_PORT } = require('./const')
-const { callZome, createAgent, startHappInterface, listInstalledApps, installHostedHapp } = require("./api")
+const { callZome, createAgent, listInstalledApps, installHostedHapp } = require('./api')
 const { parsePreferences, formatBytesByUnit } = require('./utils')
-const { getAppIds, getReadOnlyPubKey} = require('./const')
-const { AdminWebsocket, AppWebsocket } = require("@holochain/conductor-api")
+const { getAppIds, getReadOnlyPubKey } = require('./const')
+const { AdminWebsocket, AppWebsocket } = require('@holochain/conductor-api')
+yargs(hideBin(process.argv))
 
-app.get('/hosted_happs', async (time_interval, res) => {
+app.get('/hosted_happs', async (timeInterval, res) => {
   let happs
-  const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`);
+  const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`)
   try {
     const APP_ID = await getAppIds()
     happs = await callZome(appWs, APP_ID.HHA, 'hha', 'get_happs', null)
-  } catch(e) {
-      console.log("error from /hosted_happs:", e);
-      return res.status(501).send(`hpos-holochain-api error: ${e}`);
+  } catch (e) {
+    console.log('error from /hosted_happs:', e)
+    return res.status(501).send(`hpos-holochain-api error: ${e}`)
   }
   const presentedHapps = []
-  for(let i=0; i < happs.length; i++) {
-    let app_stats, enabled, source_chains, duration, bandwidth, cpu;
-    try{
+  for (let i = 0; i < happs.length; i++) {
+    let appStats, enabled, sourceChains, duration, bandwidth, cpu, usage
+    try {
       // nb: servicelogger bandwidth payload is calcalated with Bytes (not bits)
-      app_stats = await callZome(appWs,`${happs[i].happ_id}::servicelogger`, 'service', 'get_happ_usage', time_interval);
+      appStats = await callZome(appWs, `${happs[i].happ_id}::servicelogger`, 'service', 'get_happ_usage', timeInterval)
       enabled = true
-    } catch(e) {
-      throw new Error(`Error calling get_stats from ${happs[i].happ_id}::servicelogger : `, e);
+    } catch (e) {
+      throw new Error(`Error calling get_stats from ${happs[i].happ_id}::servicelogger : `, e)
     }
-    
-    if (!app_stats) {
+
+    if (!appStats) {
       enabled = false
-      source_chains = 0
+      sourceChains = 0
       usage = {}
     } else {
-      ({ source_chain_count: source_chains, duration, bandwidth, cpu } = app_stats);
-      const formatted_bandwidth = formatBytesByUnit(bandwidth); // format bandwidth into object with highest appropriate unit of measurement and respective size (ie: { size: 1, unit: GB })
-      bandwidth = formatted_bandwidth;
+      ({ source_chain_count: sourceChains, duration, bandwidth, cpu } = appStats)
+      bandwidth = formatBytesByUnit(bandwidth) // format bandwidth into object with highest appropriate unit of measurement and respective size (ie: { size: 1, unit: GB })
+      usage = {
+        duration,
+        bandwidth,
+        cpu
+      }
     }
 
     presentedHapps.push({
-        id: happs[i].happ_id,
-        name: happs[i].happ_bundle.name,
-        enabled,
-        source_chains,
-        usage: {
-          duration,
-          bandwidth,
-          cpu
-        }
+      id: happs[i].happ_id,
+      name: happs[i].happ_bundle.name,
+      enabled,
+      sourceChains,
+      usage
     })
   }
   res.status(200).send(presentedHapps)
@@ -67,7 +67,7 @@ app.post('/install_hosted_happ', async (req, res) => {
 
   // check if happ_id is passed else return error
   if (data.happ_id && data.preferences) {
-    let happId = data.happ_id;
+    const happId = data.happ_id
     // preferences: {
     //   max_fuel_before_invoice: "5", // how much holofuel to accumulate before sending invoice
     //   price_compute: "1",
@@ -75,62 +75,59 @@ app.post('/install_hosted_happ', async (req, res) => {
     //   price_bandwidth: "1",
     //   max_time_before_invoice: [86400, 0], // how much time to allow to pass before sending invoice even if fuel trigger not reached.
     // }
-    let preferences = data.preferences;
-    if (!preferences.max_fuel_before_invoice
-      || !preferences.max_time_before_invoice
-      || !preferences.price_compute
-      || !preferences.price_storage
-      || !preferences.price_bandwidth) {
-        console.log("wrong preferences...");
-        return res.status(501).send(`hpos-holochain-api error: preferences does not include all the necessary values`);
+    const preferences = data.preferences
+    if (!preferences.max_fuel_before_invoice ||
+      !preferences.max_time_before_invoice ||
+      !preferences.price_compute ||
+      !preferences.price_storage ||
+      !preferences.price_bandwidth) {
+      console.log('wrong preferences...')
+      return res.status(501).send(`hpos-holochain-api error: preferences does not include all the necessary values`)
     }
-    console.log("Trying to install happ with happId: ", happId)
+    console.log('Trying to install happ with happId: ', happId)
 
     // Steps:
     // - Call hha to get happ details
-    let happBundleDetails;
+    let happBundleDetails
     try {
       const APP_ID = await getAppIds()
-      const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`);
+      const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`)
       happBundleDetails = await callZome(appWs, APP_ID.HHA, 'hha', 'get_happ', happId)
-
     } catch (e) {
-      return res.status(501).send(`hpos-holochain-api error: ${e}`);
+      return res.status(501).send(`hpos-holochain-api error: ${e}`)
     }
-    console.log("Happ Bundle: ", happBundleDetails);
-    let happAlias = happBundleDetails.happ_bundle.happ_alias;
+    console.log('Happ Bundle: ', happBundleDetails)
 
-    let listOfInstalledHapps;
+    let listOfInstalledHapps
     // Instalation Process:
     try {
-      const adminWs = await AdminWebsocket.connect(`ws://localhost:${ADMIN_PORT}`);
+      const adminWs = await AdminWebsocket.connect(`ws://localhost:${ADMIN_PORT}`)
       // Do we need to make sure app interface is started?
       // await startHappInterface(adminWs);
 
-      listOfInstalledHapps = await listInstalledApps(adminWs);
+      listOfInstalledHapps = await listInstalledApps(adminWs)
 
       // Generate new agent in a test environment else read the location in hpos
-      const hostPubKey = process.env.NODE_ENV === 'test' ? await createAgent(adminWs) : await getReadOnlyPubKey();
+      const hostPubKey = process.env.NODE_ENV === 'test' ? await createAgent(adminWs) : await getReadOnlyPubKey()
 
       // Install DNAs
-      let dnas = happBundleDetails.happ_bundle.dnas;
+      const dnas = happBundleDetails.happ_bundle.dnas
 
       // check if the hosted_happ is already listOfInstalledHapps
       if (listOfInstalledHapps.includes(`${happBundleDetails.happ_id}`)) {
-        return res.status(501).send(`hpos-holochain-api error: ${happBundleDetails.happ_id} already installed on your holoport`);
+        return res.status(501).send(`hpos-holochain-api error: ${happBundleDetails.happ_id} already installed on your holoport`)
       } else {
         const serviceloggerPref = parsePreferences(preferences, happBundleDetails.provider_pubkey)
-        console.log("Parsed Preferences: ", serviceloggerPref);
+        console.log('Parsed Preferences: ', serviceloggerPref)
         await installHostedHapp(happBundleDetails.happ_id, dnas, hostPubKey, serviceloggerPref)
       }
       // Note: Do not need to install UI's for hosted happ
-      return res.status(200).send(`Successfully installed happ_id: ${happId}`);
+      return res.status(200).send(`Successfully installed happ_id: ${happId}`)
     } catch (e) {
-      return res.status(501).send(`hpos-holochain-api error: Failed to install hosted Happ with error - ${e}`);
+      return res.status(501).send(`hpos-holochain-api error: Failed to install hosted Happ with error - ${e}`)
     }
-  }
-  else {
-  return res.status(501).send(`hpos-holochain-api error: Failed to pass happId in body`);
+  } else {
+    return res.status(501).send(`hpos-holochain-api error: Failed to pass happId in body`)
   }
 })
 
@@ -146,4 +143,4 @@ app.listen(UNIX_SOCKET, () => {
   console.log(`Host console server running`)
 })
 
-module.exports = {app}
+module.exports = { app }
