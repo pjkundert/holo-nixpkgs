f0f68264eadad6fec24ae7285961e7618a0d3187
Robbie Carlton <robbie.carlton@gmail.com>
2021-02-22 13:08:22 -0700

    hpos-holochain-api conforms to standardjs

--- overlays/holo-nixpkgs/hpos-holochain-api/src/index.js
+++ overlays/holo-nixpkgs/hpos-holochain-api/src/index.js
@@ -5,38 +5,38 @@ const express = require('express')
 const app = express()
 const yargs = require('yargs/yargs')
 const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+yargs(hideBin(process.argv))
 const { UNIX_SOCKET, HAPP_PORT, ADMIN_PORT } = require('./const')
-const { callZome, createAgent, startHappInterface, listInstalledApps, installHostedHapp } = require("./api")
+const { callZome, createAgent, listInstalledApps, installHostedHapp } = require('./api')
 const { parsePreferences } = require('./utils')
-const { getAppIds, getReadOnlyPubKey} = require('./const')
-const { AdminWebsocket, AppWebsocket } = require("@holochain/conductor-api")
+const { getAppIds, getReadOnlyPubKey } = require('./const')
+const { AdminWebsocket, AppWebsocket } = require('@holochain/conductor-api')
 
 app.get('/hosted_happs', async (_, res) => {
   let happs
-  const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`);
+  const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`)
   try {
     const APP_ID = await getAppIds()
     happs = await callZome(appWs, APP_ID.HHA, 'hha', 'get_happs', null)
-  } catch(e) {
-      console.log("error from /hosted_happs:", e);
-      return res.status(501).send(`hpos-holochain-api error: ${e}`);
+  } catch (e) {
+    console.log('error from /hosted_happs:', e)
+    return res.status(501).send(`hpos-holochain-api error: ${e}`)
   }
   const presentedHapps = []
-  for(let i=0; i < happs.length; i++) {
-    let enabled, source_chain;
-    try{
-      source_chain = await callZome(appWs,`${happs[i].happ_id}::servicelogger`, 'service', 'get_source_chain_count', null)
+  for (let i = 0; i < happs.length; i++) {
+    let enabled, sourceChain
+    try {
+      sourceChain = await callZome(appWs, `${happs[i].happ_id}::servicelogger`, 'service', 'get_source_chain_count', null)
       enabled = true
-    } catch(e) {
+    } catch (e) {
       enabled = false
-      source_chain = 0
+      sourceChain = 0
     }
     presentedHapps.push({
-        id: happs[i].happ_id,
-        name: happs[i].happ_bundle.name,
-        enabled,
-        source_chain
+      id: happs[i].happ_id,
+      name: happs[i].happ_bundle.name,
+      enabled,
+      source_chain: sourceChain
     })
   }
   res.status(200).send(presentedHapps)
@@ -51,7 +51,7 @@ app.post('/install_hosted_happ', async (req, res) => {
 
   // check if happ_id is passed else return error
   if (data.happ_id && data.preferences) {
-    let happId = data.happ_id;
+    const happId = data.happ_id
     // preferences: {
     //   max_fuel_before_invoice: "5", // how much holofuel to accumulate before sending invoice
     //   price_compute: "1",
@@ -59,62 +59,59 @@ app.post('/install_hosted_happ', async (req, res) => {
     //   price_bandwidth: "1",
     //   max_time_before_invoice: [86400, 0], // how much time to allow to pass before sending invoice even if fuel trigger not reached.
     // }
-    let preferences = data.preferences;
-    if (!preferences.max_fuel_before_invoice
-      || !preferences.max_time_before_invoice
-      || !preferences.price_compute
-      || !preferences.price_storage
-      || !preferences.price_bandwidth) {
-        console.log("wrong preferences...");
-        return res.status(501).send(`hpos-holochain-api error: preferences does not include all the necessary values`);
+    const preferences = data.preferences
+    if (!preferences.max_fuel_before_invoice ||
+      !preferences.max_time_before_invoice ||
+      !preferences.price_compute ||
+      !preferences.price_storage ||
+      !preferences.price_bandwidth) {
+      console.log('wrong preferences...')
+      return res.status(501).send(`hpos-holochain-api error: preferences does not include all the necessary values`)
     }
-    console.log("Trying to install happ with happId: ", happId)
+    console.log('Trying to install happ with happId: ', happId)
 
     // Steps:
     // - Call hha to get happ details
-    let happBundleDetails;
+    let happBundleDetails
     try {
       const APP_ID = await getAppIds()
-      const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`);
+      const appWs = await AppWebsocket.connect(`ws://localhost:${HAPP_PORT}`)
       happBundleDetails = await callZome(appWs, APP_ID.HHA, 'hha', 'get_happ', happId)
-
     } catch (e) {
-      return res.status(501).send(`hpos-holochain-api error: ${e}`);
+      return res.status(501).send(`hpos-holochain-api error: ${e}`)
     }
-    console.log("Happ Bundle: ", happBundleDetails);
-    let happAlias = happBundleDetails.happ_bundle.happ_alias;
+    console.log('Happ Bundle: ', happBundleDetails)
 
-    let listOfInstalledHapps;
+    let listOfInstalledHapps
     // Instalation Process:
     try {
-      const adminWs = await AdminWebsocket.connect(`ws://localhost:${ADMIN_PORT}`);
+      const adminWs = await AdminWebsocket.connect(`ws://localhost:${ADMIN_PORT}`)
       // Do we need to make sure app interface is started?
       // await startHappInterface(adminWs);
 
-      listOfInstalledHapps = await listInstalledApps(adminWs);
+      listOfInstalledHapps = await listInstalledApps(adminWs)
 
       // Generate new agent in a test environment else read the location in hpos
-      const hostPubKey = process.env.NODE_ENV === 'test' ? await createAgent(adminWs) : await getReadOnlyPubKey();
+      const hostPubKey = process.env.NODE_ENV === 'test' ? await createAgent(adminWs) : await getReadOnlyPubKey()
 
       // Install DNAs
-      let dnas = happBundleDetails.happ_bundle.dnas;
+      const dnas = happBundleDetails.happ_bundle.dnas
 
       // check if the hosted_happ is already listOfInstalledHapps
       if (listOfInstalledHapps.includes(`${happBundleDetails.happ_id}`)) {
-        return res.status(501).send(`hpos-holochain-api error: ${happBundleDetails.happ_id} already installed on your holoport`);
+        return res.status(501).send(`hpos-holochain-api error: ${happBundleDetails.happ_id} already installed on your holoport`)
       } else {
         const serviceloggerPref = parsePreferences(preferences, happBundleDetails.provider_pubkey)
-        console.log("Parsed Preferences: ", serviceloggerPref);
+        console.log('Parsed Preferences: ', serviceloggerPref)
         await installHostedHapp(happBundleDetails.happ_id, dnas, hostPubKey, serviceloggerPref)
       }
       // Note: Do not need to install UI's for hosted happ
-      return res.status(200).send(`Successfully installed happ_id: ${happId}`);
+      return res.status(200).send(`Successfully installed happ_id: ${happId}`)
     } catch (e) {
-      return res.status(501).send(`hpos-holochain-api error: Failed to install hosted Happ with error - ${e}`);
+      return res.status(501).send(`hpos-holochain-api error: Failed to install hosted Happ with error - ${e}`)
     }
-  }
-  else {
-  return res.status(501).send(`hpos-holochain-api error: Failed to pass happId in body`);
+  } else {
+    return res.status(501).send(`hpos-holochain-api error: Failed to pass happId in body`)
   }
 })
 
@@ -130,4 +127,4 @@ app.listen(UNIX_SOCKET, () => {
   console.log(`Host console server running`)
 })
 
-module.exports = {app}
+module.exports = { app }
--- overlays/holo-nixpkgs/hpos-holochain-api/src/utils.js
+++ overlays/holo-nixpkgs/hpos-holochain-api/src/utils.js
@@ -1,37 +1,36 @@
-const tmp = require("tmp")
-const request = require("request")
-const url = require("url")
-const fs = require("fs")
+const tmp = require('tmp')
+const request = require('request')
+const fs = require('fs')
 
 // Download from url to tmp file
 // return tmp file path
 const downloadFile = async (downloadUrl) => {
-    console.log("Downloading url: ", downloadUrl);
-    const fileName = tmp.tmpNameSync();
-    let file = fs.createWriteStream(fileName);
+  console.log('Downloading url: ', downloadUrl)
+  const fileName = tmp.tmpNameSync()
+  const file = fs.createWriteStream(fileName)
 
-    // Clean up url
-    let urlObj = new URL(downloadUrl);
-    urlObj.protocol = "https";
-    downloadUrl = urlObj.toString();
+  // Clean up url
+  const urlObj = new URL(downloadUrl)
+  urlObj.protocol = 'https'
+  downloadUrl = urlObj.toString()
 
-    return new Promise((resolve, reject) => {
-        let stream = request({
-            uri: downloadUrl
-        })
-        .pipe(file)
-        .on('finish', () => {
-            //console.log(`Downloaded file from ${downloadUrl} to ${fileName}`);
-            resolve(fileName);
-        })
-        .on('error', (error) => {
-            reject(error);
-        })
+  return new Promise((resolve, reject) => {
+    request({
+      uri: downloadUrl
     })
+      .pipe(file)
+      .on('finish', () => {
+        // console.log(`Downloaded file from ${downloadUrl} to ${fileName}`);
+        resolve(fileName)
+      })
+      .on('error', (error) => {
+        reject(error)
+      })
+  })
 }
 
 const parsePreferences = (p, key) => {
-  const mtbi = typeof p.max_time_before_invoice == "string" ? JSON.parse(p.max_time_before_invoice) :p.max_time_before_invoice
+  const mtbi = typeof p.max_time_before_invoice === 'string' ? JSON.parse(p.max_time_before_invoice) : p.max_time_before_invoice
   return {
     max_fuel_before_invoice: toInt(p.max_fuel_before_invoice),
     max_time_before_invoice: [toInt(mtbi[0]), toInt(mtbi[1])],
@@ -43,7 +42,7 @@ const parsePreferences = (p, key) => {
 }
 
 const toInt = (i) => {
-  if (typeof i == "string") return parseInt(i)
+  if (typeof i === 'string') return parseInt(i)
   else return i
 }
 
